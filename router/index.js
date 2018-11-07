ae5fd175546d69d339ea143fae0ac41d9a2a700c
McChen <McChen1024@163.com>
2018-11-07 14:03:09 +0800

    完成登录注销功能,session存储了登录状态

--- router/index.js
+++ router/index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const controller = require('../controller/index.js')
 
-router.get('/', (req, res) => {
-    res.render('./index.ejs', {})
-})
+router.get('/',controller.handleIndexGet)
 
 module.exports = router
