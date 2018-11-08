465f8496ee9ccc05b03cc4d596b14dac2d1e89b7
McChen <McChen1024@163.com>
2018-11-08 19:38:20 +0800

    文章详情页功能完成,404页面完成,编辑功能完成,编辑按钮的权限控制,根据用户id和文章所属人id来判断

--- controller/article.js
+++ controller/article.js
@@ -1,5 +1,7 @@
 const moment = require('moment')
 const conn = require('../db/db.js')
+const marked = require('marked')
+
 module.exports = {
     handleArticleAddGet(req, res) {
         //判断用户登录状态 处理登录拦截
@@ -22,9 +24,53 @@ module.exports = {
         })
     },
     handleArticleInfoGet(req, res) {
-        res.render('./articles/info.ejs', {
-            user: req.session.user,
-            isLogin: req.session.isLogin
+        //req.query用于获取? 传递过来的参数
+        //req.params 用户获取 :id 传递过来的参数
+        // console.log(req.params)
+        const id = req.params.id
+        const querySql = 'select * from articles where id = ?'
+        conn.query(querySql, id, (err, result) => {
+            //封装渲染的对象,登录用户信息和是否登录的记录
+            const renderObj = {
+                user: req.session.user,
+                isLogin: req.session.isLogin
+            }
+            //如果出现错误或者查不到文章就返回404页面
+            if(err || result.length !== 1) return res.render('./404.ejs',renderObj)
+            //如果找到文章就将文章转换为HTML标签
+            result[0].content = marked(result[0].content)
+            // console.log(result[0])
+            //将文章对象加到renderObj中
+            renderObj.article = result[0]
+            //渲染文章详情页面
+            res.render('./articles/info.ejs', renderObj)
+        })
+    },
+    handleArticleEditGet(req, res) {
+        //判断是否登录
+       if(!req.session.isLogin) return res.redirect('/')
+        //查询数据库 根据ID获取文章信息
+        const id = req.params.id
+        const querySql = 'select * from articles where id = ?'
+        conn.query(querySql, id, (err, result) => {
+            if(err || result.length !== 1) return res.status(500).send({status: 500, msg: "文章获取失败,请重试!", data: null})
+            // console.log(result);
+            res.render('./articles/edit.ejs', {
+                user: req.session.user,
+                isLogin: req.session.isLogin,
+                article: result[0]
+            })
+            
+        })
+    },
+    handleArticleEditPost(req, res) {
+        const article = req.body
+        article.ctime = moment().format('YYYY-MM-DD HH:mm:ss')
+        // console.log(article)
+        const updateSql = 'update articles set ? where id = ?'
+        conn.query(updateSql, [article, article.id], (err, result) => {
+            if(err || result.affectedRows !== 1) return res.status(400).send({status: 400, msg: "修改文章失败,请重试", data: null})
+            res.send({status:200, articleId: article.id});
         })
     }
 }
